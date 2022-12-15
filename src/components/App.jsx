473a17c712b854d173ca1b643aaa79cc20d17691
sonyaaat <sofiatkach2004@gmail.com>
2022-12-15 16:33:09 +0200

    Routes fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 import Menu from './Menu/Menu';
 import Layout from './Layout/Layout';
 import Basket from './Basket/Basket';
@@ -7,6 +7,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/menu"/>}/>
         <Route path="menu" element={<Menu />} />
         <Route path="basket" element={<Basket />} />
       </Route>
